def2befd693631721e4defa2979397e8fa3a68b1
“Potato <samson.park1@gmail.com>
2021-03-24 21:02:13 -0400

    game button alignment

--- client/src/components/gameComponents/GameBoard.js
+++ client/src/components/gameComponents/GameBoard.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import GameTile from "./GameTile";
 import Grid from "../../gameLogic/Grid";
+import Timer from "./Timer";
 
-const GameBoard = ({ startTimer, openWinNotification, openLossNotification }) => {
+const GameBoard = ({ startTimer, openWinNotification, openLossNotification, started, showModal }) => {
   const [firstClick, setFirstClick] = useState(true);
   const [grid, setGrid] = useState(new Grid(18, 14, 5));
   const [tilesData, setTilesData] = useState(grid.cells);
@@ -73,7 +74,17 @@ const GameBoard = ({ startTimer, openWinNotification, openLossNotification }) =>
 
   return (
     <div>
-      <button className="new-game-button" type="button" onClick={resetBoard}>New Game</button>
+      <div className="game-info-container">
+        <div className="get-started-button">
+          <div className="rounded-button-extra button large" onClick={showModal}>
+            Game Rules
+          </div>
+        </div>  
+        <Timer started={started} />
+        <div className="rounded-button-extra button large" onClick={resetBoard}>
+          New Game
+        </div>
+      </div>
       <div className="game-board">
         {tiles}
       </div>
